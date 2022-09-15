3793137517764d0646a0d9ce4406c5169886d53d
jarlion <oncwnuGDsmFDRh2yNfHZy98KOdSI@git.weixin.qq.com>
2022-09-16 00:51:44 +0800

    # 重构 html 元素

--- src/utils/htmlUtil.ts
+++ src/utils/htmlUtil.ts
@@ -15,8 +15,9 @@ export function createHtml(body: string): string {
 
 export type IHtmlChildren = IHTMLElement | IHTMLElement[] | string | null;
 
-export function html(child: IHTMLElement): IHTMLElement {
-    return new HtmlContainer('html', child).setComment('<!doctype html>');
+export function html(title: string = '', lang: string = 'zh-CN'): HTML {
+    // return new HtmlContainer('html', child).setComment('<!doctype html>');
+    return new HTML(title, lang);
 }
 
 export function makeTable<T extends Record<string, string>>(data: T[], eachFn: (colName: string, row: number) => string, indents: string = ''): HtmlContainer {
@@ -36,7 +37,7 @@ export function makeTable<T extends Record<string, string>>(data: T[], eachFn: (
         let prop = '';
         const cls = eachFn && eachFn(prop, i);
         // 添加序号
-        row.push(td(i).setClass(cls ? cls + ' index' : 'index'));
+        row.push(td(i.toString()).setClass(cls ? cls + ' index' : 'index'));
 
         for (let j = 0; j < cols.length; j++) {
             prop = cols[j];
@@ -89,18 +90,18 @@ ${indents}${indents}<tr>`;
     return table;
 }
 
-export function div(children: IHtmlChildren, indents: string = ''): HtmlContainer {
+export function div(children: IHtmlChildren = null, indents: string = ''): HtmlContainer {
     return new HtmlContainer('div', children, indents);
 }
 
-export function span(children: IHtmlChildren, indents: string = ''): HtmlContainer {
+export function span(children: IHtmlChildren = null, indents: string = ''): HtmlContainer {
     return new HtmlContainer('span', children, indents);
 }
 
-export function table(children: IHtmlChildren, indents: string = ''): HtmlContainer {
+export function table(children: IHtmlChildren = null, indents: string = ''): HtmlContainer {
     return new HtmlContainer('table', children, indents);
 }
-export function tr(children: IHtmlChildren, indents: string = ''): HtmlContainer {
+export function tr(children: IHtmlChildren = null, indents: string = ''): HtmlContainer {
     return new HtmlContainer('tr', children, indents);
 }
 export function th(children: IHtmlChildren = null, indents: string = ''): HtmlContainer {
@@ -113,6 +114,11 @@ export function td(children: IHtmlChildren = null, indents: string = ''): HtmlCo
 export function img(src: string, indents: string = ''): HTMLImage {
     return new HTMLImage(src, indents);
 }
+
+export function link(href: string, type = "text/css", rel = "stylesheet"): HTMLLink {
+    return new HTMLLink(href, type, rel);
+}
+
 
 /**
  * 缩进
@@ -139,22 +145,46 @@ export class Indent {
 }
 
 export interface IHTMLElement {
+    tag: string;
+    parent?: IHTMLElement;
     toString(): string;
 }
 
+export class HTMLText implements IHTMLElement {
+    tag: string = '';
+    parent?: IHTMLElement | undefined;
+    constructor(public text: string) { }
+    toString(): string {
+        return this.text;
+    }
+
+}
+
 export class HTMLBaseElement implements IHTMLElement {
+
+    parent?: IHTMLElement = undefined;
+
     protected _comment: string = '';
-    protected _id: string = '';
-    protected _class: string = '';
-    protected _style: string = '';
+
+    protected _attributes: Record<string, string> = {};
 
     constructor(public tag: string, public indents: string = '') { }
-    getAttributes(): Record<string, string> {
-        return {
-            id: this._id,
-            className: this._class,
-            style: this._style
-        };
+    // getAttributes(): Record<string, string> {
+    //     return {
+    //         id: this._id,
+    //         className: this._class,
+    //         style: this._style
+    //     };
+    // }
+
+    setAttribute(name: string, value: string): this {
+        this._attributes[name] = value;
+        return this;
+    }
+    appendAttribute(name: string, value: string): this {
+        if (!value) return this;
+        this._attributes[name] = `${this._attributes[name]} ${value}`;
+        return this;
     }
 
     setComment(value: string): this {
@@ -163,23 +193,24 @@ export class HTMLBaseElement implements IHTMLElement {
     }
 
     setId(value: string): this {
-        this._id = value;
+        this.setAttribute('id', value);
         return this;
     }
 
-    setClass(value: string): this {
-        this._class = value;
-        return this;
-    }
-
-    setStyle(value: string): this {
-        this._style = value;
+    appendListener(event: string, methodName: string): this {
+        this.setAttribute(event, methodName);
         return this;
     }
 
     toString(): string {
-        let startTag = this._getStartTag(this.tag);
-        return `${this._getIndents()}<${startTag} />`;
+        const comment = this._comment;
+        const startTag = this._getStartTag(this.tag);
+        const content = this._formContent();
+        const indents = this._getIndents();
+        const result = `${comment}\n${indents}`;
+        if (content === '') return `${result}<${startTag} />`;
+
+        return `${result}<${startTag}>${content}</${this.tag}>`;
     }
 
     protected _getIndents(): string {
@@ -189,36 +220,141 @@ export class HTMLBaseElement implements IHTMLElement {
 
     protected _getStartTag(tag: string): string {
         let startTag = tag;
-        if (this._id) startTag += ` id="${this._id}"`;
-        if (this._class) startTag += ` class="${this._class}"`;
-        if (this._style) startTag += ` style="${this._style}"`;
+        startTag += this._formatAttribute();
         return startTag;
+    }
+
+    protected _formatAttribute(): string {
+        let attrs = '';
+        for (const name in this._attributes) {
+            attrs += ` ${name}="${this._attributes[name]}"`;
+        }
+        return attrs;
+    }
+
+    protected _formContent(): string {
+        return '';
+    }
+}
+
+export class HtmlStyleElement extends HTMLBaseElement {
+    setClass(value: string): this {
+        this.setAttribute('class', value);
+        return this;
+    }
+
+    appendClass(value: string): this {
+        return this.appendAttribute('class', value);
+    }
+
+    setStyle(value: string): this {
+        this.setAttribute('style', value);
+        return this;
+    }
+    appendStyle(value: string): this {
+        return this.appendAttribute('style', value);;
     }
 }
 
 /**
  * HTML 元素模板
  */
-export class HtmlContainer extends HTMLBaseElement {
-    constructor(tag: string, public child: IHtmlChildren = null, indents: string = '') {
+export class HtmlContainer extends HtmlStyleElement {
+    protected _children: IHTMLElement[] = [];
+    constructor(tag: string, child: IHtmlChildren = null, indents: string = '') {
         super(tag, indents);
+        if (typeof child === 'string') {
+            this._children.push(new HTMLText(child));
+        }
+        else if (Array.isArray(child)) {
+            this._children.push(...child);
+        }
+        else {
+            if (child !== null) this._children.push(child);
+        }
     }
 
-    toString(): string {
-        let startTag = this._getStartTag(this.tag);
-        const children = Array.isArray(this.child) ? (this.child as IHTMLElement[]).reduce<IHTMLElement>((p, c) => p + c.toString(), '') : this.child?.toString() ?? '';
-        const indents = this._getIndents();
-        return `${indents}<${startTag}>${children}${indents}</${this.tag}>`;
+    append(child: IHTMLElement, index = -1): this {
+        if (!child) return this;
+        child.parent = this;
+        let start = index >= 0 ? index : this._children.length + index + 1;
+        this._children.splice(start, 0, child);
+        return this;
+    }
+
+    protected _formContent(): string {
+        const content = this._children.reduce((p, c) => p + c.toString(), '');
+        return content;
+    }
+
+}
+
+export class HTMLMeta extends HTMLBaseElement {
+    constructor(name: string = '', content = '', charset = "utf-8") {
+        super('meta');
+        this.setAttribute('charset', charset)
+            .setAttribute('name', name)
+            .setAttribute('content', content);
+    }
+}
+export class HTMLLink extends HTMLBaseElement {
+    constructor(href: string, type = "text/css", rel = "stylesheet") {
+        super('link');
+        this.setAttribute('href', href)
+            .setAttribute('type', type)
+            .setAttribute('rel', rel);
+    }
+}
+
+export class HTML extends HtmlContainer {
+    constructor(title: string = '', lang: string = 'zh-CN') {
+        super('html', [
+            new HtmlContainer('head')
+                .append(new HTMLMeta('', '', 'utf-8'))
+                .append(new HTMContent('title', title)),
+            new HtmlContainer('body')]);
+        this.setAttribute('lang', lang)
+            .setComment('<!DOCTYPE html>');
+    }
+
+    head(): HtmlContainer {
+        return this._children[0] as HtmlContainer;
+    }
+    body(): HtmlContainer {
+        return this._children[1] as HtmlContainer;
+    }
+
+    append(child: IHTMLElement, index: number = -1): this {
+        if (['link', 'meta', 'title'].includes(child.tag)) {
+            this.head().append(child, index);
+        }
+        else {
+            this.body().append(child, index);
+        }
+        return this;
     }
 }
 
 export class HTMLImage extends HTMLBaseElement {
-    constructor(public src: string, indents: string = '') {
+    constructor(src: string, indents: string = '') {
         super('img', indents);
+        this.setAttribute('src', src);
+    }
+}
+
+export class HTMContent extends HTMLBaseElement {
+    constructor(tag: string, protected _content: string, indents: string = '') {
+        super(tag, indents);
     }
 
-    protected _getStartTag(tag: string): string {
-        let startTag = super._getStartTag(tag);
-        return `${startTag} src=${this.src}`;
+    protected _formContent(): string {
+        return this._content;
+    }
+}
+
+export class HTMScript extends HTMContent {
+    constructor(content: string, type = 'text/javascript', indents: string = '') {
+        super('script', content, indents);
+        this.setAttribute('type', type);
     }
 }
