a7984e1f8d467523e43a7854abdadbfd6fda24f9
jarlion <oncwnuGDsmFDRh2yNfHZy98KOdSI@git.weixin.qq.com>
2022-09-18 04:53:48 +0800

    # 补齐表头数据

--- src/utils/xlsxUtil.ts
+++ src/utils/xlsxUtil.ts
@@ -1,6 +1,14 @@
 import { readFile, Sheet2JSONOpts, utils } from 'xlsx';
 
-export function readXlsx<R>(path: string, sheetIndex: number = 0, opts?: Sheet2JSONOpts | undefined): R[] {
+/**
+ * 读取 xlsx
+ * @param path 
+ * @param sheetIndex 
+ * @param opts 
+ * @param fix 是否在没有配置的时候把表头添加到数据中
+ * @returns 
+ */
+export function readXlsx<R extends Record<string, string>>(path: string, sheetIndex: number = 0, opts?: Sheet2JSONOpts | undefined, fix: boolean = true): R[] {
 
     const wb = readFile(path);
     const sheetName: string = wb.SheetNames[sheetIndex];
@@ -10,5 +18,11 @@ export function readXlsx<R>(path: string, sheetIndex: number = 0, opts?: Sheet2J
     }
     const sheet = wb.Sheets[sheetName];
     const rows = utils.sheet_to_json<R>(sheet, opts);
+    // 补齐表头行
+    if (rows.length > 0 && !opts && fix) {
+        const head: Record<string, string> = {};
+        Object.keys(rows).forEach(prop => head[prop] = prop);
+        rows.unshift(head as R);
+    }
     return rows;
 }
